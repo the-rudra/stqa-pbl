7b0ad19d59c45d777eb034b943eff8b669d49097
rudra <itsrudrapratapdev@gmail.com>
2023-10-11 18:58:00 +0530

    initial commit

---
+++ features/login/Login.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+import styles from "./login.module.scss";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const Router = useRouter();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1",
+      );
+      setUser(data);
+      Router.push("/dashboard");
+    } catch {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <span className={styles.user}>{user.name}</span>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          className={styles.input}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className={styles.button}
+          disabled={!username || !password}
+          onClick={handleClick}
+        >
+          {loading ? "please wait" : "Login"}
+        </button>
+        <span
+          data-testid="error"
+          className={styles.button}
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/index.jsx
@@ -0,0 +1,29 @@
+import { Routes } from "@config/routes";
+import styles from "./index.module.scss";
+import Login from "../features/login/Login";
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/logo-large.svg" alt="Prolog logo" />
+        <a href={Routes.projects}>Dashboard</a>
+      </header>
+      <button
+        className={styles.contactButton}
+        onClick={() =>
+          alert(
+            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
+          )
+        }
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/message.svg" alt="Contact" />
+      </button>
+      <Login />
+    </div>
+  );
+};
+
+export default IssuesPage;
